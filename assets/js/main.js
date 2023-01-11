b2a90e13d7c6f0619cc2c719958fbf70813f7cda
Aarzoo <59678435+Aarzoo75@users.noreply.github.com>
2023-01-11 13:33:19 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/*===== MENU SHOW =====*/ 
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+
